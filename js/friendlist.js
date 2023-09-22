5adcfa069a071b2f34f9bdc2e6ea95c202426f8c
Surmani-Konsam <Surmanikonsam96@gmail.com>
2023-09-23 01:10:31 +0530

    friendlist-review-comment:completed
    - removed all innerHtml and implement dom manipulation.
    - used forEach((element,index)) to track over the element position.
    - used classList.add for css styling.
    - added comments on the css.
    - added comments on the html markup.
    - flex-wrap has been removed.
    - no positions have been used.
    - ux to ui is matched.

--- js/friendlist.js
+++ js/friendlist.js
@@ -1,64 +1,91 @@
-
 // selecting friendList container
 const friendListSpace = document.querySelector(".friendList");
-
+const fragment = document.createDocumentFragment();
 
 //fetching json file using fetch api
 fetch("friends.json")
   .then((response) => {
-
     //if response is not ok then throw error
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     return response.json();
   })
-  .then((data) => { //if no error then json file will be iterated via forEach loop 
-    data.forEach((element) => {
+  .then((data) => {
+    let rowDiv;
+    //if no error then json file will be iterated via forEach loop
+    data.forEach((element, index)  => {
+
+      if (index % 3 === 0) {
+        rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+        fragment.appendChild(rowDiv);
+      }
 
       //creating div element.
       const divTag = document.createElement("div");
 
-      //setting created element with class name.
-      divTag.setAttribute("class", "friend_list_child");
-      const divTagCssText =  "width: 507px; height : 120px; background-color: #4d66b4; margin-bottom: 15px;";
-      divTag.style.cssText = divTagCssText; //setting css style to the div tag created div tag.
+      divTag.classList.add('friend_list_child');
+
+      const listContainerTag = document.createElement('div');
+      listContainerTag.classList.add('list_container');
+
+      const imageSpaceTag = document.createElement('div');
+      imageSpaceTag.classList.add('image_space');
       
-      //creating markup structure for the value received from the json file.
-      const innerHTMLText =
-        `<div class='list_container'>` +
-        `<div class='image_space'>` +
-        `<img style='width:80px; height: 80px; border-radius: 50%' src='${element.img}' alt='friend list img'>` +
-        `</div>` +
-        `<div class='friend_name'>` +
-        `<div class='name' style='color: white'>${element.first_name} ${element.last_name}</div>` +
-        `<div class='email_id'>${element.email}</div>` +
-        `</div>` +
-        `</div>`;
-      //assigned the markup to the divTag innerhtml
-      divTag.innerHTML = innerHTMLText;
+      const imageTag = document.createElement('img');
+      imageTag.classList.add('image_container');
+      imageTag.src = `${element.img}`;
+      imageTag.alt = 'friend list img';
+
+      imageSpaceTag.appendChild(imageTag);
+
+      const friendNameTag = document.createElement('div');
+      friendNameTag.classList.add('friend_name');
+
+
+      const nameDivTag = document.createElement('div');
+      nameDivTag.classList.add('name');
+      nameDivTag.textContent = `${element.first_name} ${element.last_name}`;
+
+
+      const emailDivTag = document.createElement('div');
+      emailDivTag.classList.add('email_id');
+      emailDivTag.textContent = `${element.email}`;
+
+      friendNameTag.appendChild(nameDivTag);
+      friendNameTag.appendChild(emailDivTag);
+
+      listContainerTag.appendChild(imageSpaceTag);
+      listContainerTag.appendChild(friendNameTag);
+
+      divTag.appendChild(listContainerTag);
+
 
       //selecting .list_container from the markup created.
       const friendListClass = divTag.querySelector(".list_container");
-      const friendListCssText =  "display : flex; margin-top: 20px; box-sizing: border-box;padding-left: 20px;";
-      friendListClass.style.cssText = friendListCssText; //adding style to the list_container
+      friendListClass.classList.add('list_container_style');;
 
       //selecting .name from the markup created
-      const friendName = divTag.querySelector('.name');
-      const friendNameCssText = "color: #ffff; font-size: 21px";
-      friendName.style.cssText = friendNameCssText; //adding style to the .name 
+      const friendName = divTag.querySelector(".name");
+      friendName.classList.add('name_style');
 
       //selecting .email_id from the markup created.
-      const emailId = divTag.querySelector('.email_id');
-      const emailIdCssText = "color: #ffff;font-weight: 300";
-      emailId.style.cssText = emailIdCssText; //adding style to the email_id for ux to ui match
+      const emailId = divTag.querySelector(".email_id");
+      emailId.classList.add('email_id_style');
+      
 
-      const cssText =
-        "box-sizing: border-box;margin-left: 20px; display: flex;flex-direction: column;justify-content: space-evenly;height: 60px;margin-top: 10px;";
-      divTag.querySelector(".friend_name").style.cssText = cssText; //adding style to the friend name for ux to ui match.
-      friendListSpace.append(divTag); //divTag will be appended iteratively to the parent container friendListSpace until last value of the json file read.
+      // const cssText =
+      //   "box-sizing: border-box;margin-left: 20px; display: flex;flex-direction: column;justify-content: space-evenly;height: 60px;margin-top: 10px;";
+      // divTag.querySelector(".friend_name").style.cssText = cssText; //adding style to the friend name for ux to ui match.
+       //divTag will be appended iteratively to the parent container friendListSpace until last value of the json file read.
+      rowDiv.appendChild(divTag);
+      fragment.appendChild(rowDiv)
+
     });
+    friendListSpace.append(fragment); //will be added only one time to the parent dom.
   })
-  .catch((error) => { //if there is error in reading the json file will catch the error and log it to the console.
+  .catch((error) => {
+    //if there is error in reading the json file will catch the error and log it to the console.
     console.error("Error fetching the JSON file:", error);
   });
