4c133a05fad8469fe44b38a8abf5cef20ad46785
Surmani-Konsam <Surmanikonsam96@gmail.com>
2023-09-22 23:33:30 +0530

    btn-click-review:completed

--- js/btnclick.js
+++ js/btnclick.js
@@ -1,11 +1,47 @@
+import { paymentMethods } from "./constants.js";
+
 //all the dom elements
 let shapes = document.querySelector(".shapes");
 let next_btn = document.querySelector(".nxt_btn");
-const circleTick = document.querySelector(".tick");
-const triangleTick = document.querySelector(".triangle_tick");
-const squareTick = document.querySelector(".square_tick");
-const header = document.querySelector(".learning_header");
-const mainContent = document.querySelector(".learning_content");
+let header = document.querySelector(".learning_header");
+let mainContent = document.querySelector(".learning_content");
+
+//for triggering click event when clicked over the displaye shapes.
+document.querySelector(".circle").addEventListener("click", function () {
+  toggleTick(this, ".circle");
+});
+
+//for triggering click event when clicked over the displaye shapes.
+document.querySelector(".triangle").addEventListener("click", function () {
+  toggleTick(this, ".triangle");
+});
+
+//for triggering click event when clicked over the displaye shapes.
+document.querySelector(".square").addEventListener("click", function () {
+  toggleTick(this, ".square");
+});
+
+//toggleTick function
+function toggleTick(shape, shapeName) {
+  const ticks = document.querySelectorAll(".tick");
+  ticks.forEach((tick) => {
+    tick.style.opacity = "0";
+  });
+  const tick = shape.querySelector(".tick"); //again selecting to make the opacity set to 1.
+  tick.style.opacity = "1"; //this will make the check image opacity from 0 to 1 with a transition set to 0.3 secs.
+  nextBtn(shapeName); //here concept of closure will come... it will search for value of currentShape in its parent environmennt.
+}
+//end of toggleTick function
+
+//nextBtn function
+function nextBtn(shapeName) {
+  shape["name"] = shapeName;
+  next_btn.textContent = "NEXT";
+  next_btn.classList.add("next_btn_style");
+  //this event will be removed inside the function calculateAreaAndObjectOfTheShape
+  next_btn.addEventListener("click", calculateAreaAndObjectOfTheShape);
+}
+//end of nextBtn function
 
 //object for storage.
 //provided if we add one more figure we need to only define the area of the figure and its respective perimeter formula
@@ -46,106 +82,45 @@ let shape = {
 };
 //end of our global object.
 
-//function to reload the page all again bind to btn 'btn_reload'.
-function reload() {
-  location.reload();
-}
-
-//table content created for displaying in the final page after retrieving the value from the local storage.
-function tableContent() {
-  if (shape.name === ".triangle") {
-    //retrieving the object from the local storage and parse it back to object.
-    const objectRetrieved = JSON.parse(localStorage.getItem(".triangle"));
-
-    //defining table structure for the shapes.
-    mainContent.innerHTML =
-      `<div class='last_page'> ` +
-      `<div class='last_page_triangle'></div>` +
-      `<h1 class='triangle_header'>Equilateral Triangle</h1>` +
-      `<div class='row_container'>` +
-      `<div class='first_row row'>` +
-      `<div class='box first_column'>SIDE</div>` +
-      `<div class='middle box'>S</div>` +
-      `<div class='length box last_column'>${objectRetrieved.side}</div>` +
-      `</div>` +
-      `<div class='second_row row'>` +
-      `<div class='area box first_column'>AREA</div>` +
-      `<div class='middle box'>0.43 * S * S</div>` +
-      `<div class='area_msr box last_column'>${objectRetrieved.area}</div>` +
-      `</div>` +
-      `<div class='third_row row'>` +
-      `<div class='perimeter box first_column'>PERIMETER</div>` +
-      `<div class='middle box'>3 * S</div>` +
-      `<div class='perim_value box last_column'>${objectRetrieved.perimeter}</div>` +
-      `</div>` +
-      `<button class='btn_reload' onclick='reload()'>START AGAIN</button>`;
-    ("</div>");
-  } else if (shape.name === ".circle") {
-    //retrieving the object from the local storage and parse it back to object.
-    const objectRetrieved = JSON.parse(localStorage.getItem(".circle"));
-    mainContent.innerHTML =
-      `<div class='last_page'> ` +
-      `<div class='last_page_circle'></div>` +
-      `<h1 class='circle_header'>CIRCLE</h1>` +
-      `<div class='row_container'>` +
-      `<div class='first_row row'>` +
-      `<div class='box first_column'>RADIUS</div>` +
-      `<div class='middle box'>r</div>` +
-      `<div class='length box last_column'>${objectRetrieved.radius}</div>` +
-      `</div>` +
-      `<div class='second_row row'>` +
-      `<div class='area box first_column'>AREA</div>` +
-      `<div class='middle box'>πr²</div>` +
-      `<div class='area_msr box last_column'>${objectRetrieved.area}</div>` +
-      `</div>` +
-      `<div class='third_row row'>` +
-      `<div class='perimeter box first_column'>PERIMETER</div>` +
-      `<div class='middle box'>2πr</div>` +
-      `<div class='perim_value box last_column'>${objectRetrieved.perimeter}</div>` +
-      `</div>` +
-      `<button class='btn_reload' onclick='reload()'>START AGAIN</button>`;
-    ("</div>");
-  } else {
-    //retrieving the object from the local storage and parse it back to object.
-    const objectRetrieved = JSON.parse(localStorage.getItem(".square"));
-    mainContent.innerHTML =
-      `<div class='last_page'> ` +
-      `<div class='last_page_square'></div>` +
-      `<h1 class='square_header'>SQUARE</h1>` +
-      `<div class='row_container'>` +
-      `<div class='first_row row'>` +
-      `<div class='box first_column'>SIDE</div>` +
-      `<div class='middle box'>S</div>` +
-      `<div class='length box last_column'>${objectRetrieved.side}</div>` +
-      `</div>` +
-      `<div class='second_row row'>` +
-      `<div class='area box first_column'>AREA</div>` +
-      `<div class='middle box'>S * S</div>` +
-      `<div class='area_msr box last_column'>${objectRetrieved.area}</div>` +
-      `</div>` +
-      `<div class='third_row row'>` +
-      `<div class='perimeter box first_column'>PERIMETER</div>` +
-      `<div class='middle box'>4 * S</div>` +
-      `<div class='perim_value box last_column'>${objectRetrieved.perimeter}</div>` +
-      `</div>` +
-      `<button class='btn_reload' onclick='reload()'>START AGAIN</button>`;
-    ("</div>");
-  }
-}
 
 //common function for reuse in the nextBtn function.
 function calculateAreaAndObjectOfTheShape() {
   //if triangle
   if (shape["name"] === ".triangle") {
-    header.textContent = "2. Enter Side (Base & Height)";
+    header.textContent = paymentMethods.filter(
+      (x) => x.key === ".triangle"
+    )[0].header;
 
-    const trianglCssTextContent =
-      "display:inline-block; text-align:center; color : white";
-    shapes.style.cssText = trianglCssTextContent;
+    shapes.classList.add("shapes_triangle_style");
 
-    const triangleInnerHtml =
-      "Enter Base <input type='number' class='base'/> <div  class='height_parent'>Enter Height <input type='number'  class='height'/></div>";
-    shapes.innerHTML = triangleInnerHtml;
+    const baseTextNode = document.createTextNode("Enter Base ");
+    const triangleInputTag = document.createElement("input");
+    triangleInputTag.setAttribute("type", "number");
+    triangleInputTag.classList.add("base");
+    triangleInputTag.id = "triangle_input";
+
+    const heightParentDiv = document.createElement("div");
+    heightParentDiv.classList.add("height_parent");
+
+    const heightLabel = document.createTextNode("Enter Height ");
+
+    // Create the input field for height
+    const heightInput = document.createElement("input");
+    heightInput.setAttribute("type", "number");
+    heightInput.classList.add("height");
+    heightInput.id = "triangle_height";
+
+    // Append the label and input to the 'height_parent' div
+    heightParentDiv.appendChild(heightLabel);
+    heightParentDiv.appendChild(heightInput);
+
+
+    shapes.innerHTML = "";
+
+    shapes.appendChild(baseTextNode);
+    shapes.appendChild(triangleInputTag);
+    shapes.appendChild(heightParentDiv);
+
 
     next_btn.textContent = "CALCULATE";
 
@@ -170,14 +145,23 @@ function calculateAreaAndObjectOfTheShape() {
   }
   //if circle
   else if (shape["name"] === ".circle") {
-    header.textContent = "2. Enter Radius";
+    header.textContent = paymentMethods.filter(
+      (x) => x.key === ".circle"
+    )[0].header;
 
-    const circleCssTextContent =
-      "display:inline-block; text-align:center; color : white";
-    shapes.style.cssText = circleCssTextContent;
 
-    const circleInnerHtml = "<input type='number' class='radius' style='' /> ";
-    shapes.innerHTML = circleInnerHtml;
+    // shapes.classList.add("shape_circle_style");
+    const circleInputTag = document.createElement('input');
+    circleInputTag.setAttribute('type','number');
+    circleInputTag.classList.add('radius');
+    circleInputTag.id = 'circle_input';
+
+    shapes.innerHTML = '';
+    shapes.append(circleInputTag);
+
+    // const circleInnerHtml =
+    //   "<input type='number' class='radius' id='circle_input'/> ";
+    // shapes.innerHTML = circleInnerHtml;
 
     next_btn.textContent = "CALCULATE";
 
@@ -197,12 +181,22 @@ function calculateAreaAndObjectOfTheShape() {
   }
   //if Square
   else {
-    header.textContent = "2. Enter Side";
-    const squareCssTextContent =
-      "display:inline-block; text-align:center; color : white";
-    shapes.style.cssText = squareCssTextContent;
-    const squareInnerHtml = "<input type='number' class='side' />";
-    shapes.innerHTML = squareInnerHtml;
+    header.textContent = paymentMethods.filter(
+      (x) => x.key === ".square"
+    )[0].header;
+    shapes.classList.add("shape_square_style");
+
+    const squareInputTag = document.createElement('input');
+    squareInputTag.type = 'number';
+    squareInputTag.classList.add('side');
+    squareInputTag.id = 'square_input';
+
+    shapes.innerHTML = '';
+    shapes.append(squareInputTag);
+
+    // const squareInnerHtml =
+    //   "<input type='number' class='side' id='square_input'/>";
+    // shapes.innerHTML = squareInnerHtml;
 
     next_btn.textContent = "CALCULATE";
 
@@ -223,41 +217,167 @@ function calculateAreaAndObjectOfTheShape() {
 }
 //end of calculateAreaAndObjectOfTheShape().
 
-//nextBtn function
-function nextBtn(shapeName) {
-  shape["name"] = shapeName;
-  next_btn.textContent = "NEXT";
-  const cssTextContent =
-    "border : 2px solid white; background-color: black; color: white; padding-top : 8px; box-sizing: border-box; text-align: center; cursor: pointer";
-  next_btn.style.cssText = cssTextContent;
-  //this event will be removed inside the function calculateAreaAndObjectOfTheShape
-  next_btn.addEventListener("click", calculateAreaAndObjectOfTheShape);
+//table content created for displaying in the final page after retrieving the value from the local storage.
+function tableContent() {
+  if (shape.name === ".triangle") {
+    //retrieving the object from the local storage and parse it back to object.
+    let objectRetrieved = JSON.parse(localStorage.getItem(".triangle"));
+    let side = objectRetrieved.side;
+    innerHtmlContent(
+      "Equilateral Triangle",
+      "SIDE",
+      "0.433 * S * S",
+      "3 * S",
+      "s",
+      objectRetrieved,
+      side,
+      "last_page_triangle"
+    );
+  } else if (shape.name === ".circle") {
+    //retrieving the object from the local storage and parse it back to object.
+    let objectRetrieved = JSON.parse(localStorage.getItem(".circle"));
+    let side = objectRetrieved.radius;
+    innerHtmlContent(
+      "Circle",
+      "RADIUS",
+      "π r2",
+      "2 π r",
+      "r",
+      objectRetrieved,
+      side,
+      "last_page_circle"
+    );
+  } else {
+    //retrieving the object from the local storage and parse it back to object.
+    let objectRetrieved = JSON.parse(localStorage.getItem(".square"));
+    let side = objectRetrieved.side;
+    innerHtmlContent(
+      "Square",
+      "SIDE",
+      "S * S",
+      "4 * S",
+      "s",
+      objectRetrieved,
+      side,
+      "last_page_square"
+    );
+  }
 }
-//end of nextBtn function
 
-//toggleTick function
-function toggleTick(shape, shapeName) {
-  const ticks = document.querySelectorAll(".tick");
-  ticks.forEach((tick) => {
-    tick.style.opacity = "0";
-  });
-  const tick = shape.querySelector(".tick"); //again selecting to make the opacity set to 1.
-  tick.style.opacity = "1"; //this will make the check image opacity from 0 to 1 with a transition set to 0.3 secs.
-  nextBtn(shapeName); //here concept of closure will come... it will search for value of currentShape in its parent environmennt.
+//function to reload the page all again bind to btn 'btn_reload'.
+function reload() {
+  location.reload();
 }
-//end of toggleTick function
 
-//for triggering click event when clicked over the displaye shapes.
-document.querySelector(".circle").addEventListener("click", function () {
-  toggleTick(this, ".circle");
-});
+function innerHtmlContent(
+  shapeHeader,
+  shapeSide,
+  area,
+  perimeter,
+  sideSymbol,
+  objectRetrieved,
+  side,
+  shapeClass
+) {
+  console.log(objectRetrieved);
+  const lastPageDiv = document.createElement("div");
+  lastPageDiv.classList.add("last_page");
 
-//for triggering click event when clicked over the displaye shapes.
-document.querySelector(".triangle").addEventListener("click", function () {
-  toggleTick(this, ".triangle");
-});
+  // Create a div element with the specified shapeClass
+  const shapeDiv = document.createElement("div");
+  shapeDiv.classList.add(shapeClass);
 
-//for triggering click event when clicked over the displaye shapes.
-document.querySelector(".square").addEventListener("click", function () {
-  toggleTick(this, ".square");
-});
+  // Create an h1 element with class 'triangle_header' and set the shapeHeader text
+  const headerElement = document.createElement("h1");
+  headerElement.classList.add("triangle_header");
+  headerElement.textContent = shapeHeader;
+
+  // Create a div element with class 'row_container'
+  const rowContainerDiv = document.createElement("div");
+  rowContainerDiv.classList.add("row_container");
+
+  // Create first row elements
+  const firstRowDiv = document.createElement("div");
+  firstRowDiv.classList.add("first_row", "row");
+
+  const firstColumnDiv = document.createElement("div");
+  firstColumnDiv.classList.add("box", "first_column");
+  firstColumnDiv.textContent = shapeSide;
+
+  const middleDiv = document.createElement("div");
+  middleDiv.classList.add("middle", "box");
+  middleDiv.textContent = sideSymbol;
+  // console.log('side symbol',sideSymbol); everything is good till here.
+
+  const lastColumnDiv = document.createElement("div");
+  lastColumnDiv.classList.add("length", "box", "last_column");
+  lastColumnDiv.textContent = side;
+  // console.log('side :',side);
+
+  //first row all appended here
+  firstRowDiv.appendChild(firstColumnDiv);
+  firstRowDiv.appendChild(middleDiv);
+  firstRowDiv.appendChild(lastColumnDiv);
+
+  // Create second row elements
+  const secondRowDiv = document.createElement("div");
+  secondRowDiv.classList.add("second_row", "row");
+
+  const areaColumnDiv = document.createElement("div");
+  areaColumnDiv.classList.add("area", "box", "first_column");
+  areaColumnDiv.textContent = "AREA";
+
+  const areaMiddleDiv = document.createElement("div");
+  areaMiddleDiv.classList.add("middle", "box");
+  areaMiddleDiv.textContent = area;
+
+  const areaValueDiv = document.createElement("div");
+  areaValueDiv.classList.add("area_msr", "box", "last_column");
+  areaValueDiv.textContent = objectRetrieved.area;
+
+  secondRowDiv.appendChild(areaColumnDiv);
+  secondRowDiv.appendChild(areaMiddleDiv);
+  secondRowDiv.appendChild(areaValueDiv);
+
+  // Create third row elements
+  const thirdRowDiv = document.createElement("div");
+  thirdRowDiv.classList.add("third_row", "row");
+
+  const perimeterColumnDiv = document.createElement("div");
+  perimeterColumnDiv.classList.add("perimeter", "box", "first_column");
+  perimeterColumnDiv.textContent = "PERIMETER";
+
+  const perimeterMiddleDiv = document.createElement("div");
+  perimeterMiddleDiv.classList.add("middle", "box");
+  perimeterMiddleDiv.textContent = perimeter;
+
+  const perimeterValueDiv = document.createElement("div");
+  perimeterValueDiv.classList.add("perim_value", "box", "last_column");
+  perimeterValueDiv.textContent = objectRetrieved.perimeter;
+
+  thirdRowDiv.appendChild(perimeterColumnDiv);
+  thirdRowDiv.appendChild(perimeterMiddleDiv);
+  thirdRowDiv.appendChild(perimeterValueDiv);
+
+  // Create the reload button
+  const reloadButton = document.createElement("button");
+  reloadButton.classList.add("btn_reload");
+  reloadButton.textContent = "START AGAIN";
+  reloadButton.addEventListener("click", reload);
+
+  // Append the elements in the correct order
+  rowContainerDiv.appendChild(firstRowDiv);
+  rowContainerDiv.appendChild(secondRowDiv);
+  rowContainerDiv.appendChild(thirdRowDiv);
+  rowContainerDiv.appendChild(reloadButton);
+
+  lastPageDiv.appendChild(shapeDiv);
+  lastPageDiv.appendChild(headerElement);
+  lastPageDiv.appendChild(rowContainerDiv);
+
+  // Clear mainContent before appending
+  mainContent.innerHTML = "";
+
+  // Append lastPageDiv to mainContent
+  mainContent.appendChild(lastPageDiv);
+}
