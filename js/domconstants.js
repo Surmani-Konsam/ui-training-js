6da4da2ebdc997ddcf843313e103ec2e0027d986
Surmani-Konsam <Surmanikonsam96@gmail.com>
2023-10-05 19:51:07 +0530

    friendlist-updated
    - modules added for dom query elements and fetch function api.
    - used one common module.

---
+++ js/domconstants.js
@@ -0,0 +1,5 @@
+// selecting friendList container
+const friendListSpace = document.querySelector(".friendList");
+const fragment = document.createDocumentFragment();
+
+export { fragment, friendListSpace };
