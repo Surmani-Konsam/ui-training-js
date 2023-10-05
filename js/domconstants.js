0ea06f9a21c6c0e295d3e0efbc821a2739ee691a
Surmani-Konsam <Surmanikonsam96@gmail.com>
2023-10-05 19:45:19 +0530

    btn-click-updated
    - used modules for the constants and function.
    - created one common module for all the constants and functions use.
    - padding was used with short-hand

---
+++ js/domconstants.js
@@ -0,0 +1,6 @@
+let shapes = document.querySelector(".shapes");
+let next_btn = document.querySelector(".nxt_btn");
+let header = document.querySelector(".learning_header");
+let mainContent = document.querySelector(".learning_content");
+
+export { shapes, next_btn, header, mainContent };
--- js/paymentConstants.js
+++ js/paymentConstants.js
@@ -16,3 +16,6 @@ const paymentMethods = [
 ];
 
 export { paymentMethods };
+
+
+
